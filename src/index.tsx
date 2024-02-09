ae15e0aede2746d82be31b06b6a92e8d8ac2b364
costajpb <costa.jpb@gmail.com>
2024-02-09 20:54:16 +0100

    fix: show details as home page

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TVShow from './pages/tv-show';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <TVShow id={1} />
+    <App />
   </React.StrictMode>
 );
 
