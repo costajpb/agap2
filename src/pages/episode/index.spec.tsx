9d7b572895f2f4f879ca3b356102d6af2867655f
costajpb <costa.jpb@gmail.com>
2024-02-11 20:39:24 +0100

    test (pages): episode without summary and back to show navigation

--- src/pages/episode/index.spec.tsx
+++ src/pages/episode/index.spec.tsx
@@ -1,23 +1,48 @@
-import { findByRole, findByTestId, render } from "@testing-library/react"
+import { findByRole, findByTestId, fireEvent, getByText, render, waitFor } from "@testing-library/react"
 import Episode from "."
 import data from '../../domain/tv-show/repository/__fixtures__/tvshow.json'
 import Entity from "../../domain/episode/entity"
 
+const mockReturn = jest.fn()
+
+jest.mock('../../application/episode/index', () => {
+    return class {
+        return () {
+            mockReturn()
+        }
+    }
+})
+
 const rawEpisode = data._embedded.episodes[0]
 
 describe('pages/episode', () => {
-    it('should show title, summary and cover image', async () => {
-        const episode: Entity = {
-            id: rawEpisode.id,
-            title: rawEpisode.name,
-            summary: rawEpisode.summary,
-            coverImage: rawEpisode.image.original
-        }
+    const episode: Entity = {
+        id: rawEpisode.id,
+        title: rawEpisode.name,
+        summary: rawEpisode.summary,
+        coverImage: rawEpisode.image.original
+    }
 
+    it('should show title, summary and cover image', async () => {
         const { container } = render(<Episode details={episode} />)
 
         expect((await findByRole(container, 'heading')).textContent).toBe(episode.title)
         expect((await findByTestId(container, 'summary')).textContent != '').toBeTruthy()
         expect((await findByRole(container, 'img')).getAttribute('src')).toBe(episode.coverImage)
+    })
+
+    it('should render with a message when summary is not available', () => {
+        const { container } = render(<Episode details={{...episode, summary: undefined}} />)
+
+        expect(container).toMatchSnapshot()
+    })
+
+    test('return to show page', async () => {
+        const { container } = render(<Episode details={episode} />)
+        const anchor = getByText(container, /return/i)
+
+        fireEvent.click(anchor)
+
+        await waitFor(() => expect(mockReturn).toHaveBeenCalledTimes(1))
     })
 })
