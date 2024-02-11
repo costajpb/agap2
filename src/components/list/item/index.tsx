6c5eadcc46b66b95dc2aeb10014b4ce48361db5c
costajpb <costa.jpb@gmail.com>
2024-02-11 12:25:23 +0100

    refactor (pages): tv show episodes as a self-container component

--- src/components/list/item/index.tsx
+++
@@ -1,9 +0,0 @@
-import { x } from "@xstyled/styled-components"
-
-type ItemProps = {
-    children: React.ReactNode
-}
-
-export default function Item({children}: ItemProps) {
-    return (<x.li h={{_: 24, sm: 32, md: 24, lg: 32}} bg="gray-200" borderRadius="2xl" position="relative" overflow="hidden">{children}</x.li>)
-}
