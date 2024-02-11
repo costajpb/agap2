13b50aa893e2f325447a51b6c7ea6ea75709bdaf
costajpb <costa.jpb@gmail.com>
2024-02-11 12:31:34 +0100

    test (components): page title snapshot

---
+++ src/components/page-title/index.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react"
+import PageTitle from "."
+
+describe('components/page-title', () => {
+    it('should render', () => {
+        const { container } = render(<PageTitle>Page title</PageTitle>)
+        expect(container).toMatchSnapshot()
+    })
+})
